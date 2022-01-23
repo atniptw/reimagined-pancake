02b5c320b9a4af597bf58d3b02e1c5a04e0b4153
Tom Atnip <tom.atnip@gmail.com>
2022-01-23 14:02:46 -0500

    Comment out App Insights and APIM

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -25,35 +25,35 @@ data "azurerm_resource_group" "rg" {
   name     = "PDAZE1APIMRG01"
 }
 
-resource "azurerm_application_insights" "ai" {
-  name                = "pdaze1apimai01"
-  resource_group_name = "${data.azurerm_resource_group.rg.name}"
-  location            = "${data.azurerm_resource_group.rg.location}"
-  application_type    = "other"
-  retention_in_days   = 30
-}
+# resource "azurerm_application_insights" "ai" {
+#   name                = "pdaze1apimai01"
+#   resource_group_name = "${data.azurerm_resource_group.rg.name}"
+#   location            = "${data.azurerm_resource_group.rg.location}"
+#   application_type    = "other"
+#   retention_in_days   = 1
+# }
 
-resource "azurerm_api_management" "apim" {
-  name                = "pdaze1apimam01"
-  resource_group_name = "${data.azurerm_resource_group.rg.name}"
-  location            = "${data.azurerm_resource_group.rg.location}"
-  publisher_name      = "Professional Development"
-  publisher_email     = "atnip@sep.com"
+# resource "azurerm_api_management" "apim" {
+#   name                = "pdaze1apimam01"
+#   resource_group_name = "${data.azurerm_resource_group.rg.name}"
+#   location            = "${data.azurerm_resource_group.rg.location}"
+#   publisher_name      = "Professional Development"
+#   publisher_email     = "atnip@sep.com"
 
-  sku_name = "Consumption_0"
+#   sku_name = "Consumption_0"
 
-  policy {
-    xml_content = <<XML
-    <policies>
-      <inbound />
-      <backend />
-      <outbound />
-      <on-error />
-    </policies>
-XML
+#   policy {
+#     xml_content = <<XML
+#     <policies>
+#       <inbound />
+#       <backend />
+#       <outbound />
+#       <on-error />
+#     </policies>
+# XML
 
-  }
-}
+#   }
+# }
 
 resource "azurerm_app_service_plan" "webappserviceplan" {
   name                = "api-appserviceplan-pro"
